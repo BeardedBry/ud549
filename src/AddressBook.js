09fb6e314bb8a019ba87c4b2e1bc353baaa3abc4
Brian Burkard <brianburkard@gmail.com>
2018-11-16 19:58:24 -0500

    passed the add contact test

---
+++ src/AddressBook.js
@@ -0,0 +1,12 @@
+function AddressBook() {
+
+};
+
+AddressBook.prototype.addContact = function(a){
+    return this.a = a
+}
+
+AddressBook.prototype.getContact = function(e){
+    let objectArr = Object.values(this);
+    return objectArr[e];
+}
